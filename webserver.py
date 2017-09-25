f3b0640d3df6045d8cb3d2c9d5fa42d0641aeedb
Chike Ezeh <ezeh.chike@gmail.com>
2017-09-25 07:21:59 -0500

    adds new restaurant to database

--- webserver.py
+++ webserver.py
@@ -23,17 +23,58 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                 output = ""
                 output += "<html><body>"
+                # creates a link for adding a new restaurant
+                output += "<a href='/restaurants/new'> Create A new Restaurant</br></br></a>"
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "</br>"
                     output += "<a href ='#'>Edit</a></br>"
                     output += "<a href ='#'>Delete</a></br></br>"
-                output += "</html></body>"
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                # form to accept new Restaurant
+                output += """ <form method = 'POST' enctype ='multipart/form-data'>
+                <h2>Enter A new Restaurant</h2>
+                <input name ='newRestaurantName' type ='text' placeholder ='New Restaurant'>
+                <input type ='submit' value = 'Create'></form> """
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Create new restaurant object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    # redirect to homepage
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+        except IOError:
+            pass
 
 
 def main():
